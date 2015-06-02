a3a1b8b0c50a6a5f1757b1640553f76106f0c5b4
Chris Grimmett <xtoast@gmail.com>
2015-06-02 23:21:10 +0000

    making start script executable

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 sudo mkdir -p /opt/ezstream
 sudo cp ./sampleconfig.xml /opt/ezstream/stream.xml
 sudo cp ./entry.sh /opt/ezstream/start.sh
+sudo chmod +x /opt/ezstream/start.sh
 sudo apt-get update && sudo apt-get install -y \
     sox libsox-fmt-all ezstream lame \
     --no-install-recommends 
