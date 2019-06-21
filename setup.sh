471a495b8d51871c2c3caf8281eefa095dd4008f
Chris Grimmett <chris@grimtech.net>
2019-06-20 23:34:43 -0700

    add ansible role

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+## DO NOT USE. USE ANSIBLE, YOU DUMMY!
+
+
 sudo mkdir -p /opt/noaa
 sudo cp ./examplestream.liq. /opt/noaa/stream.liq
 sudo cp ./liquidsoap.conf /etc/init/liquidsoap.conf
 sudo apt-get update && sudo apt-get install -y \
-    liquisoap liquidsoap-plugin-all \
+    liquidsoap liquidsoap-plugin-all \
     --no-install-recommends 
     
